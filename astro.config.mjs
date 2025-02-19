f04c203d90971cad7e1d461ad4eb2b6c08106583
bohdanx <Bohd4n@proton.me>
2025-02-19 04:50:42 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-	integrations: [tailwind(), react()],
-	output: "server",
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
